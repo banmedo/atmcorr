01d694d77ed4c8783832f9952e056b82bf2bfe5e
Nishanta Khanal <nkhanal@icimod.org>
2018-01-25 16:55:40 +0545

    WIP - need to adapt get correct Image to work with map ID

--- AtmCorr/atmos/helpers/GetCorrectedImage.py
+++ AtmCorr/atmos/helpers/GetCorrectedImage.py
@@ -2,10 +2,9 @@
 '''
 Performs atmospheric correction and returns the corrected object
 '''
-def getCorrectedImage (credentials,args):
+def getCorrectedImage (credentials,mapID):
     import ee
     ee.Initialize(credentials)
-
 
     import os
     import sys
@@ -18,4 +17,53 @@ def getCorrectedImage (credentials,args):
     from radiance import radiance_from_TOA
     from interpolated_LUTs import Interpolated_LUTs
 
-    return args
+    # a place and a mission
+    geom = ee.Geometry.Point(77.8574, 30.2211)
+    mission = 'COPERNICUS/S2'
+
+    se = SixS_emulator(mission)
+
+    ic = ee.ImageCollection(mission)\
+        .filterBounds(geom)\
+        .filterDate('2016-06-01','2016-06-30')\
+        .filter(ee.Filter.lt('MEAN_SOLAR_ZENITH_ANGLE',75))
+
+    iLUTs = Interpolated_LUTs(mission)
+
+    iLUTs.interpolate_LUTs()
+    se.iLUTs = iLUTs.get()
+
+    # extract atmcorr inputs as feature collection
+    Atmcorr_input.geom = geom  # specify target location (would use image centroid otherwise)
+    atmcorr_inputs = ic.map(Atmcorr_input.extractor).getInfo()
+    features = atmcorr_inputs['features']
+
+    # atmospherically correct image collection
+
+    ic_atmospherically_corrected = []
+
+    for feature in features:
+
+        # at-sensor radiance
+        toa = ee.Image(mission+'/'+feature['properties']['imgID'])
+        rad = radiance_from_TOA(toa, feature)
+
+        # 6S emulator
+        cc = se.run(feature['properties']['atmcorr_inputs'])
+
+        # atmospheric correction
+        SR = atmospheric_correction(rad, cc)
+
+        toa_id = toa.getMapId();
+        ic_atmospherically_corrected.append(SR)
+
+    # Earth Engine image collection
+    ic_atmospherically_corrected = ee.ImageCollection(ic_atmospherically_corrected)
+
+    SR = ee.Image(ic_atmospherically_corrected.first())
+    afterMapId = SR.getMapId()
+    temp = {
+        "mapId" : afterMapId['mapid'],
+        "token" : afterMapId['token']
+    }
+    return temp
--- AtmCorr/static/AtmCorr/main.js
+++ AtmCorr/static/AtmCorr/main.js
@@ -3,7 +3,7 @@ var mymap = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/banmedo/ciiibvf1k0011alki4gp6if1s/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmFubWVkbyIsImEiOiJhSklqeEZzIn0.rzfSxO3cVUhghA2sJN378A').addTo(mymap);
 
 $.ajax({
-  url:"/AtmCorr/getsrtm",
+  url:"/AtmCorr/getsomething",
   success: function(response){
     console.log(response);
   }
